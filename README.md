15fd46e51919472b3de9e7931e0425e4225c9860
Courtney828 <courtneyserongwa@gmail.com>
2020-08-19 21:21:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-hun3yd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-hun3yd)
---
+++ index.ts
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+var first = 1;
+var second = 2;
+
+console.log(lowest(first, second));
+
+ function lowest(a, b) {
+	if(a < b)
+		return a;
+	else if(a === b)
+		return 0;
+	else
+		return b;
+}
